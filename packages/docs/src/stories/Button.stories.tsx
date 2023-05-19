352883f72a8341f2bdc7a544730628450d6f8d78
Lucas Ivisson <lucasivisson1@gmail.com>
2023-05-19 13:09:43 -0300

    ✨ finishing my first story

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,7 +1,19 @@
-import { Button } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
 import type { StoryObj, Meta } from '@storybook/react'
 
 export default {
   title: 'Button',
   component: Button,
-} as Meta
+
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
