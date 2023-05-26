5e0f1bf418d471591776b39aa8d0bbad4d99ec6d
Lucas Ivisson <lucasivisson1@gmail.com>
2023-05-26 08:32:18 -0300

    ♻️ refactor styles of some components

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,40 +1,41 @@
-import { Box, Text, TextInput, TextInputProps } from "@lucas-ignite-ui/react";
-import type { StoryObj, Meta } from "@storybook/react";
+import { Box, Text, TextInput, TextInputProps } from '@lucas-ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
 
 export default {
-  title: "Form/Text Input",
+  title: 'Form/Text Input',
   component: TextInput,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   args: {},
   decorators: [
     (Story) => {
       return (
         <Box
           as="label"
-          css={{ display: "flex", flexDirection: "column", gap: "$2" }}
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
         >
           <Text size="sm">Email address</Text>
           {Story()}
         </Box>
-      );
+      )
     },
   ],
-} as Meta<TextInputProps>;
+} as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {
   args: {
-    placeholder: "Type your name",
+    placeholder: 'Type your name',
   },
-};
+}
 
 export const Disabled: StoryObj<TextInputProps> = {
   args: {
     disabled: true,
   },
-};
+}
 
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
-    prefix: "cal.com/",
+    prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
-};
+}
